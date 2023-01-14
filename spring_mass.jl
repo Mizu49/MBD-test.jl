d2af603880b86319f9bb86ac5c7cad1e6715c488
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2023-01-14 22:12:09 +0900

    SVectorを使ったデータコンテナ実装を追加

--- spring_mass.jl
+++ spring_mass.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, Plots, BenchmarkTools
+using LinearAlgebra, Plots, BenchmarkTools, StaticArrays
 
 M = 10
 C = 0
@@ -54,10 +54,22 @@ function simB(initstate, Ts, datanum)
     return statesB
 end
 
+function simC(initstate, Ts, datanum)
+
+    statesC = [SVector{2}(zeros(2)) for _ in 1:datanum]
+    statesC[1] = initstate
+    for idx = 1:datanum-1 
+        statesC[idx+1] = rk4(idx*Ts, statesC[idx], Ts)
+    end
+    
+    return statesC
+end
+
 function main()
     
     @time statesA = simA(initstate, Ts, datanum);
     @time statesB = simB(initstate, Ts, datanum);
+    @time statesC = simC(initstate, Ts, datanum);
 
     return
 end
