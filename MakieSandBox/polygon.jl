1efd93b699cb402652a771ddc2d3f2ca7ca7c1b3
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2023-01-24 21:12:17 +0900

    stanford bunny の 3D プロット

--- MakieSandBox/polygon.jl
+++ MakieSandBox/polygon.jl
@@ -1,15 +1,14 @@
-using GeometryBasics, GLMakie
+using GeometryBasics, GLMakie, FileIO
 
-
-polygon = Rect3(0.0, 0.0, 0.0, 0.75, 0.5, 0.5)
+bunny = load("Stanford_Bunny.stl")
 
 f1 = Figure()
 ax1 = Axis3(f1[1, 1]) # 3D plotする時はAxis3で3次元の軸を作る
-poly!(ax1, polygon, color=:blue, linewidth=2)
+poly!(ax1, bunny, color=:blue, linewidth=2)
 
 f2 = Figure()
 ax2 = Axis3(f2[1, 1]) # 3D plotする時はAxis3で3次元の軸を作る
-mesh!(ax2, polygon, color=:blue, linewidth=2)
+mesh!(ax2, bunny, color=:blue, linewidth=2)
 
 display(f1)
 display(f2)
