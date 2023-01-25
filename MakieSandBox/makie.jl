dc7fcead77357497b7a8fb6e46321616f3c1e58f
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2023-01-25 16:29:12 +0900

    Makieで箱のメッシュをプロットして遊んでいた

--- MakieSandBox/makie.jl
+++ MakieSandBox/makie.jl
@@ -1,11 +1,26 @@
-using GLMakie, Plots
+using Revise, GLMakie, GeometryBasics
 
-fig1 = Plots.plot()
-gr()
-Plots.plot!(fig1, rand(10), rand(10))
+# true: show in VScode plot navigator
+Makie.inline!(false)
+
+obj = Rect3(0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
+
+fig1 = Figure(resolution = (600, 600))
+ax1 = Axis3( # 3D plotする時はAxis3で3次元の軸を作る
+    fig1[1, 1],
+    title = "mesh plot",
+    xlabel = "x label",
+    ylabel = "y label",
+    zlabel = "z label",
+    elevation = pi/4,
+    azimuth   = pi/4,
+    aspect = :data,
+    viewmode = :fit,
+) 
+
+hidespines!(ax1)
+hidedecorations!(ax1)
+
+mesh!(ax1, obj, color=:blue, linewidth = 2)
 
 display(fig1)
-
-fig2 = Figure()
-Axis(fig2[1, 1])
-GLMakie.plot!(rand(10), rand(10))
