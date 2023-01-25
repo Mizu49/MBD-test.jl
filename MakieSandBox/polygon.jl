7f9167741431023136d8943ea26b46c6ff2b400c
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2023-01-25 14:29:41 +0900

    プロットの時間測定を追加

--- MakieSandBox/polygon.jl
+++ MakieSandBox/polygon.jl
@@ -2,13 +2,17 @@ using GeometryBasics, GLMakie, FileIO
 
 bunny = load("Stanford_Bunny.stl")
 
-f1 = Figure()
-ax1 = Axis3(f1[1, 1]) # 3D plotする時はAxis3で3次元の軸を作る
-poly!(ax1, bunny, color=:blue, linewidth=2)
+@time begin
+    f1 = Figure(resolution = (600, 600))
+    ax1 = Axis3(f1[1, 1]) # 3D plotする時はAxis3で3次元の軸を作る
+    poly!(ax1, bunny, color=:blue, linewidth=2)    
+end
 
-f2 = Figure()
-ax2 = Axis3(f2[1, 1]) # 3D plotする時はAxis3で3次元の軸を作る
-mesh!(ax2, bunny, color=:blue, linewidth=2)
+@time begin
+    f2 = Figure(resolution = (600, 600))
+    ax2 = Axis3(f2[1, 1]) # 3D plotする時はAxis3で3次元の軸を作る
+    mesh!(ax2, bunny, color=:blue, linewidth=2)
+end
 
 display(f1)
 display(f2)
