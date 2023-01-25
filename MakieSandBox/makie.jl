74a3712ba1bfd0f52a6d1bef585bac09ab3ca161
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2023-01-25 16:57:28 +0900

    ベクトルのプロットを追加

--- MakieSandBox/makie.jl
+++ MakieSandBox/makie.jl
@@ -3,24 +3,28 @@ using Revise, GLMakie, GeometryBasics
 # true: show in VScode plot navigator
 Makie.inline!(false)
 
-obj = Rect3(0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
+obj = Rect3(-1.0, -1.0, -1.0, 2.0, 2.0, 2.0)
 
 fig1 = Figure(resolution = (600, 600))
 ax1 = Axis3( # 3D plotする時はAxis3で3次元の軸を作る
     fig1[1, 1],
-    title = "mesh plot",
+    title  = "mesh plot",
     xlabel = "x label",
     ylabel = "y label",
     zlabel = "z label",
-    elevation = pi/4,
-    azimuth   = pi/4,
+    # elevation = pi/4,
+    # azimuth   = pi/4,
     aspect = :data,
     viewmode = :fit,
-) 
+)
 
 hidespines!(ax1)
 hidedecorations!(ax1)
 
 mesh!(ax1, obj, color=:blue, linewidth = 2)
+
+arrows!(ax1, [0, 0], [0, 0], [0, 0], [1.5, -1.5], [1.5, -1.5], [1.5, -1.5],
+    color = :red
+)
 
 display(fig1)
