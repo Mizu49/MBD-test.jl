460679b2c5d24a4793bb194dcebcc5eb40d79443
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2023-01-29 20:39:35 +0900

    キューブを回転

--- MakieSandBox/rotate_cubic.jl
+++ MakieSandBox/rotate_cubic.jl
@@ -7,18 +7,31 @@ C = [
     0 sin(theta)  cos(theta)
 ]
 
-points = [
-    Point3{Float64}(C * [ 0.5,  0.5,  0.5]),
-    Point3{Float64}(C * [-0.5,  0.5,  0.5]),
-    Point3{Float64}(C * [-0.5, -0.5,  0.5]),
-    Point3{Float64}(C * [ 0.5, -0.5,  0.5]),
-    Point3{Float64}(C * [ 0.5,  0.5, -0.5]),
-    Point3{Float64}(C * [-0.5,  0.5, -0.5]),
-    Point3{Float64}(C * [-0.5, -0.5, -0.5]),
-    Point3{Float64}(C * [ 0.5, -0.5, -0.5])
-]
+points = hcat(
+    C * [ 0.5,  0.5,  0.5],
+    C * [-0.5,  0.5,  0.5],
+    C * [-0.5, -0.5,  0.5],
+    C * [ 0.5, -0.5,  0.5],
+    C * [ 0.5,  0.5, -0.5],
+    C * [-0.5,  0.5, -0.5],
+    C * [-0.5, -0.5, -0.5],
+    C * [ 0.5, -0.5, -0.5]
+)
 
-cube = GeometryBasics.mesh(points)
+faces = [
+    1 2 3
+    3 4 1
+    1 2 6
+    1 5 6
+    1 4 5
+    4 5 8
+    3 4 7
+    4 7 8
+    5 6 7
+    5 7 8
+    2 3 6
+    3 6 7
+]
 
 fig = Figure()
 ax = Axis3(
@@ -27,8 +40,7 @@ ax = Axis3(
     # azimuth   = pi/4,
     aspect = :data,
     viewmode = :fit
-    ) # 3D plotする時はAxis3で3次元の軸を作る
-mesh!(ax, cube, color=:yellow, shading = true)
+    )
+mesh!(ax, points, faces, color=:yellow, shading = true)
 
 display(fig)
-
