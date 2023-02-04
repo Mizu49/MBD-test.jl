aa5b9c3ec245f4e072be6a207a2968ed331f9a6f
Mizu49 <mizuki.abe.r4@dc.tohoku.ac.jp>
2023-02-04 13:33:52 +0900

    add direction text

--- spacecraft/rotate_spacecraft.jl
+++ spacecraft/rotate_spacecraft.jl
@@ -4,7 +4,23 @@ include("rotation_utilities.jl")
 
 include("spacecraft_body.jl")
 
-hoge = rand(3, 3)
+label_positions = [
+    Point3{Float64}(1.0, 0.0, 0.0),
+    Point3{Float64}(-1.0, 0.0, 0.0),
+    Point3{Float64}(0.0, 1.0, 0.0),
+    Point3{Float64}(0.0, -1.0, 0.0),
+    Point3{Float64}(0.0, 0.0, 1.0),
+    Point3{Float64}(0.0, 0.0, -1.0),
+]
+
+label_texts = [
+    "FWD",
+    "AFT",
+    "PRT",
+    "STB",
+    "ZNT",
+    "NDR"
+]
 
 spacecraft = get_spacecraft_polygon()
 
@@ -13,20 +29,31 @@ points = Observable(spacecraft.points)
 fig = Figure()
 ax = Axis3(
     fig[1, 1],
-    # elevation = pi/6,
-    # azimuth   = pi/4,
+    elevation = pi/6,
+    azimuth   = pi/6,
     aspect = :data,
-    viewmode = :fit
-    )
+    viewmode = :fit,
+    limits = (-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
+)
 mesh!(ax, points, spacecraft.faces, color = :yellow ,shading = true)
 
-hidedecorations!(ax)
-hidespines!(ax)
+text!(
+    label_positions,
+    text = label_texts,
+    align = (:center, :center),
+)
 
-ratios = SVector{3}([pi/30, pi/60, 0])
+lines!(ax, [-1, 1], [0, 0], [0, 0], linestyle = :dash, color = :black, linewidth = 3)
+lines!(ax, [0, 0], [-1, 1], [0, 0], linestyle = :dash, color = :black, linewidth = 3)
+lines!(ax, [0, 0], [0, 0], [-1, 1], linestyle = :dash, color = :black, linewidth = 3)
 
-iter = 0:60
-record(fig, "spacecraft.gif", iter; framerate = 30) do idx
+# hidedecorations!(ax)
+# hidespines!(ax)
+
+ratios = SVector{3}([pi/30, 0, 0])
+
+iter = 0:300
+record(fig, "spacecraft.gif", iter; framerate = 30) do idx    
     angles = ratios .* idx
     C = euler2dcm(angles)
     points[] = C * spacecraft.points
