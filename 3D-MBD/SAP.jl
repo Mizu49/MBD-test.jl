d6b378afaab4759869464aa3130fd1213a002fe0
mizu-49 <136047182+mizu-49@users.noreply.github.com>
2023-06-09 15:58:17 +0900

    メインスクリプトを整理

--- 3D-MBD/SAP.jl
+++ 3D-MBD/SAP.jl
@@ -16,9 +16,11 @@ I2 = m2 * l2^2 / 12
 
 g = 9.81
 
+# ボディの数と一般化座標の次元を定義
 num_bodies = 1
 dim_coordinate = 7
 
+# シンボリック変数の定義
 @variables t, sym_q[1:(dim_coordinate * num_bodies)], sym_qdot[1:(dim_coordinate * num_bodies)]
 sym_q = collect(sym_q)
 sym_qdot = collect(sym_qdot)
@@ -30,12 +32,15 @@ A_1 = quaternion2dcm(sym_q[4:7])
 u_bar_1 = [0, -s1, 0]
 
 # 拘束条件のシンボリック表現
-C = [
+constraints_1 = [
     sym_q[1:3] + A_1 * u_bar_1 - (zeros(3)) # 位置に対する拘束
     transpose([1, 0, 0]) * (A_1 * [0, 1, 0]) # Revolute joint 1
     transpose([1, 0, 0]) * (A_1 * [0, 0, 1]) # Revolute joint 2
     transpose(sym_q[1:3]) * sym_q[1:3] - s1^2
 ]
+
+# 拘束条件をまとめる
+C = vcat(constraints_1)
 
 # 時間微分オペレータ
 D = Differential(t)
@@ -55,7 +60,11 @@ func_constraint = eval(build_function(C, sym_q)[1])
 func_jacobian = eval(build_function(Cq, sym_q)[1])
 func_Qc = eval(build_function(Qc, t, sym_q, sym_qdot)[1])
 
+"""
+    G_bar
 
+角速度ベクトルとクォータニオンによる姿勢表現をマップする行列
+"""
 function G_bar(q::Vector)
     
     return SMatrix{3, 4}(2 * [
@@ -156,22 +165,18 @@ end
 
 function main()
 
-    # Static simulation
-    println("Begin static analysis")
-
-    dcm = C1(pi/4)
-
-    init_transposi = dcm * [0, 0.5, 0]
-    init_eulerparam = dcm2quaternion(dcm)
-
+    # 初期値の計算
+    init_RPY = [pi/4, 0, 0]
+    init_transposi = euler2dcm(init_RPY) * [0, 0.5, 0]
+    init_eulerparam = euler2quaternion(init_RPY)
     init_q = vcat(init_transposi, init_eulerparam)
 
+    # Static simulation with Newton-Raphson method
+    print("Begin static analysis...")
     iter = 0
     isconv = false
     while (iter < 100) && isconv == false
         
-        println(iter)
-
         currentC  = func_constraint(init_q)
         currentCq = func_jacobian(init_q)
 
@@ -187,18 +192,16 @@ function main()
 
         if all(isconv_array)
             is_conv = true
-            println("finish!")
+            println("complete!")
             break
         else
             iter = iter + 1
         end
     end
 
-    println("init coordinate: $init_q")
+    println("initial coordinate: $init_q")
 
     # Dynamic simulation
-
-
     timelength = 10.0
     Ts = 1e-3
 
