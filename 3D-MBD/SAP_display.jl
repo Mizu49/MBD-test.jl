987f7fed6301b167792f53d8f6df9257ca2b1680
Mizuki ABE <mizuki.abe.mizu49@gmail.com>
2023-06-12 21:05:22 +0900

    ポリゴンの構造体を修正

--- 3D-MBD/SAP_display.jl
+++ 3D-MBD/SAP_display.jl
@@ -1,4 +1,4 @@
-using GLMakie, GeometryBasics
+using GLMakie, GeometryBasics, StaticArrays
 
 struct PlateParameters
     a::Real # x-direction length
@@ -7,27 +7,27 @@ struct PlateParameters
 end
 
 struct PlatePolygon
-    points
-    faces
+    points::SizedMatrix # mutable statically-sized array
+    faces::SMatrix # immutable statically-sized array
+    parameters::PlateParameters
 end
 
-
-function PlatePolygon(coordinate::AbstractVector, p::PlateParameters)
-   
+function PlatePolygon(coordinate::AbstractVector, parameters::PlateParameters)
+    
     C = quaternion2dcm(coordinate[4:7])
 
-    points = hcat(
-        coordinate[1:3] + C * [-p.a/2, -p.b/2, +p.t/2],
-        coordinate[1:3] + C * [-p.a/2, +p.b/2, +p.t/2],
-        coordinate[1:3] + C * [+p.a/2, +p.b/2, +p.t/2],
-        coordinate[1:3] + C * [+p.a/2, -p.b/2, +p.t/2],
-        coordinate[1:3] + C * [-p.a/2, -p.b/2, -p.t/2],
-        coordinate[1:3] + C * [-p.a/2, +p.b/2, -p.t/2],
-        coordinate[1:3] + C * [+p.a/2, +p.b/2, -p.t/2],
-        coordinate[1:3] + C * [+p.a/2, -p.b/2, -p.t/2]
-    )
+    points = SizedMatrix{3, 8}(hcat(
+        coordinate[1:3] + C * [-parameters.a/2, -parameters.b/2, +parameters.t/2],
+        coordinate[1:3] + C * [-parameters.a/2, +parameters.b/2, +parameters.t/2],
+        coordinate[1:3] + C * [+parameters.a/2, +parameters.b/2, +parameters.t/2],
+        coordinate[1:3] + C * [+parameters.a/2, -parameters.b/2, +parameters.t/2],
+        coordinate[1:3] + C * [-parameters.a/2, -parameters.b/2, -parameters.t/2],
+        coordinate[1:3] + C * [-parameters.a/2, +parameters.b/2, -parameters.t/2],
+        coordinate[1:3] + C * [+parameters.a/2, +parameters.b/2, -parameters.t/2],
+        coordinate[1:3] + C * [+parameters.a/2, -parameters.b/2, -parameters.t/2]
+    ))
     
-    faces = [
+    faces = SMatrix{12, 3}([
         1 2 3
         3 4 1
         1 2 6
@@ -40,34 +40,34 @@ function PlatePolygon(coordinate::AbstractVector, p::PlateParameters)
         5 7 8
         2 3 6
         3 6 7
-    ]
+    ])
     
-    return PlatePolygon(points, faces)
+    return PlatePolygon(points, faces, parameters)
 end
 
-function update_polygon!(polygon::PlatePolygon, p::PlateParameters, coordinate::AbstractVector)
+function update_polygon!(polygon::PlatePolygon, coordinate::AbstractVector)
 
     C = quaternion2dcm(coordinate[4:7])
 
     points = hcat(
-        coordinate[1:3] + C * [-p.a/2, -p.b/2, +p.t/2],
-        coordinate[1:3] + C * [-p.a/2, +p.b/2, +p.t/2],
-        coordinate[1:3] + C * [+p.a/2, +p.b/2, +p.t/2],
-        coordinate[1:3] + C * [+p.a/2, -p.b/2, +p.t/2],
-        coordinate[1:3] + C * [-p.a/2, -p.b/2, -p.t/2],
-        coordinate[1:3] + C * [-p.a/2, +p.b/2, -p.t/2],
-        coordinate[1:3] + C * [+p.a/2, +p.b/2, -p.t/2],
-        coordinate[1:3] + C * [+p.a/2, -p.b/2, -p.t/2]
+        coordinate[1:3] + C * [-polygon.parameters.a/2, -polygon.parameters.b/2, +polygon.parameters.t/2],
+        coordinate[1:3] + C * [-polygon.parameters.a/2, +polygon.parameters.b/2, +polygon.parameters.t/2],
+        coordinate[1:3] + C * [+polygon.parameters.a/2, +polygon.parameters.b/2, +polygon.parameters.t/2],
+        coordinate[1:3] + C * [+polygon.parameters.a/2, -polygon.parameters.b/2, +polygon.parameters.t/2],
+        coordinate[1:3] + C * [-polygon.parameters.a/2, -polygon.parameters.b/2, -polygon.parameters.t/2],
+        coordinate[1:3] + C * [-polygon.parameters.a/2, +polygon.parameters.b/2, -polygon.parameters.t/2],
+        coordinate[1:3] + C * [+polygon.parameters.a/2, +polygon.parameters.b/2, -polygon.parameters.t/2],
+        coordinate[1:3] + C * [+polygon.parameters.a/2, -polygon.parameters.b/2, -polygon.parameters.t/2]
     )
 
-    # modify array
+    # modify points of polygons
     polygon.points[:] = points
 
     return    
 end
 
 # specify the dimension of the plates
-thickness = 0.01
+thickness = 0.05
 plate1_params = PlateParameters(l1, l1, thickness)
 
 # create inital polygon
@@ -104,7 +104,7 @@ iter = 1:100:size(states, 1)
 record(fig, "animation.gif", iter, framerate = 15) do idx
 
     q = states[idx]
-    update_polygon!(plate1_polygon, plate1_params, q)
+    update_polygon!(plate1_polygon, q)
 
     ax.title = "Time: $(times[idx]) (s)"
 
